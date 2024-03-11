22c4c49849cc5bd96f48a597cb20a587c2642ffa
Gustavo Jandt Feller <fellergustavo@gmail.com>
2024-03-10 21:58:22 -0300

    Experimenting with iris data

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
-from flask import Flask, Response
+from flask import Flask, Response, request
 import json
+from sklearn import datasets
+from sklearn.decomposition import PCA
+import pandas as pd
+import numpy as np
 
 app = Flask(__name__)
-
+dr_methods = ['PCA', 'UMAP']
 
 @app.route('/')
 def hello():
@@ -10,8 +14,29 @@ def hello():
 
 @app.route('/dr-methods', methods=['GET'])
 def list_dr_methods():
-    methods = ['PCA', 'UMAP']
-    resp = Response(response=json.dumps(methods), status=200, mimetype="text/plain")
+    resp = Response(response=json.dumps(dr_methods), status=200, mimetype="text/plain")
     resp.headers['Access-Control-Allow-Origin'] = '*'
     return resp
 
+@app.route('/iris-data-dr', methods=['GET'])
+def getIrisDataDr():
+    method = request.args.get('method', default="PCA", type = str)
+    iris = datasets.load_iris()
+    X = iris.data
+    y = iris.target
+    
+    if method == "PCA":
+        pca = PCA(n_components=2)
+        pca.fit(X)
+        X = pca.transform(X)
+        print(type(X))
+        X = X.tolist()
+        #for item, idx in X:
+        #    print(item)
+        #    #item.append(y[idx])
+        
+        resp = Response(response=json.dumps(X), status=200, mimetype="text/plain")
+        resp.headers['Access-Control-Allow-Origin'] = '*'
+        return resp
+    else:
+        return "Not available method " + method
